4d9c7e7be7566563f228b57f2129bb041d28f778
Tony Nguyen <tonixsmm>
2024-04-14 16:22:23 -0700

    add framework for Epidemic

--- src/main/java/edu/gonzaga/EpdemicCard.java
+++ src/main/java/edu/gonzaga/EpdemicCard.java
@@ -2,7 +2,26 @@ package edu.gonzaga;
 
 public class EpdemicCard extends Card {
     
-    public void triggerEmpidemic(){
+    public void triggerEmpidemic() {
+        // increase the infection rate
+        increaseInfectionRate();
 
+        // infect
+        infect();
+
+        // intensify
+        intensify();
+    }
+
+    private void infect() {
+        // infect cities
+    }
+
+    private void intensify() {
+        // shuffle the discard pile and add it to the draw pile
+    }
+
+    private void increaseInfectionRate() {
+        // increase the infection rate
     }
 }
