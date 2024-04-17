ad894a354f26cc2244f1e60566c9edce37df3c71
izzytilles <123509336+izzytilles@users.noreply.github.com>
2024-04-17 11:32:45 -0700

    started on Gameboard

--- src/main/java/edu/gonzaga/Gameboard.java
+++ src/main/java/edu/gonzaga/Gameboard.java
@@ -5,15 +5,19 @@ import java.util.ArrayList;
 public class Gameboard {
     
     private ArrayList<Cure> cureList;
+    private ArrayList<City> cityList; 
     private Deck playerDeck;
     private Deck infectionDeck;
 
-    public Gameboard() {
-
+    public Gameboard(ArrayList<City> newCityList, Deck newPlayerDeck, Deck newInfectionDeck) {
+        this.cityList = newCityList;
+        this.playerDeck = newPlayerDeck;
+        this.infectionDeck = newInfectionDeck;
     }
 
 
     // Need a function for finding all cities with a research station, and making sure they don't exceed 6.
+    // Note from Izzy: I think we should implement an MVC style thing so that Gameboard is notified everytime a research station is added/removed
 
     /**
      * Gets the deck of player cards
