f2e641a7677473203de3eb141c6c93c8fd1f973c
Tony Nguyen <tonixsmm>
2024-04-28 12:11:15 -0700

    address comments and fix logic bug

--- src/main/java/edu/gonzaga/EpidemicCard.java
+++ src/main/java/edu/gonzaga/EpidemicCard.java
@@ -1,5 +1,7 @@
 package edu.gonzaga;
 
+import java.util.ArrayList;
+
 public class EpidemicCard extends Card {
     
     /**
@@ -8,7 +10,7 @@ public class EpidemicCard extends Card {
      * 
      * @Author Tony
      */
-    public void triggerEmpidemic(Gameboard board) {
+    public void triggerEpidemic(Gameboard board) {
         // increase the infection rate
         increaseInfectionRate(board);
 
@@ -20,30 +22,6 @@ public class EpidemicCard extends Card {
     }
 
     /**
-     * Infects cities
-     * 
-     * @param board the gameboard
-     * @return void
-     * @Author Tony
-     */
-    private void infect(Gameboard board) {
-        // infect cities
-        board.takeInfectionTurn();
-    }
-
-    /**
-     * Intensifies the game
-     * 
-     * @param board the gameboard
-     * @return void
-     * @Author Tony
-     */
-    private void intensify(Gameboard board) {
-        // shuffle the discard pile and add it to the draw pile
-        board.getPlayerDeck().putShuffledDiscardPileOnTop();
-    }
-
-    /**
      * Increases the infection rate
      * 
      * @param board the gameboard
@@ -51,7 +29,46 @@ public class EpidemicCard extends Card {
      * @Author Tony
      */
     private void increaseInfectionRate(Gameboard board) {
-        // increase the infection rate
         board.changeInfectionRate();
     }
+
+    /**
+     * Infects cities
+     * 
+     * @param board the gameboard
+     * @return void
+     * @Author Tony
+     */
+    private void infect(Gameboard board) {
+        // draw cards
+        ArrayList<Color> eradicatedColors = board.getEradicatedColors();
+        BasicCard cityCard = (BasicCard)board.getInfectionDeck().drawBottomCard();
+        City tempCity = cityCard.getCity();
+
+        // infect the city
+        if (!eradicatedColors.contains(tempCity.getColor())){
+            for (int i = 0; i < 3; i++) {  // i < 3 because epidemic cards only add 3 cubes
+                Integer numOutbreaks = tempCity.addInfectionCube();
+                board.setOutbreakCount(numOutbreaks);
+                tempCity.outbreakCleanup();
+            }
+        }
+
+        // discard the card
+        board.getInfectionDeck().discardCard(cityCard);
+    }
+
+    /**
+     * Intensifies the game
+     * Shuffle the discard pile and add it to the draw pile
+     * 
+     * @param board the gameboard
+     * @return void
+     * @Author Tony
+     */
+    private void intensify(Gameboard board) {
+        board.getInfectionDeck().putShuffledDiscardPileOnTop();
+    }
+
+    // TODO: remove the epidemic card from the game to prevent it from being drawn again
 }
